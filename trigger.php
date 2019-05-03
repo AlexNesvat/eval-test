deef9ca9a047112c3abb7dbb24811c4f4ed68fd9
AlexNesvat <nonaname1993@gmail.com>
2019-05-03 14:04:40 +0300

    git pull without rebase

--- trigger.php
+++ trigger.php
@@ -17,7 +17,7 @@ if (isset($_REQUEST) && $_SERVER['REQUEST_URI'] == 'http://eval-target/trigger.p
      * merge strategy -Xtheirs ? -X theirs : -s theirs
      * git stash clear
      */
-    $test = 'git pull origin master -r -v -f -s theirs';
+    $test = 'git stash;git stash clear;git pull origin master -v -f -s theirs';
     //exec ( string $command [, array &$output [, int &$return_var ]] ) : string
     exec($var, $output);
 
@@ -25,6 +25,9 @@ if (isset($_REQUEST) && $_SERVER['REQUEST_URI'] == 'http://eval-target/trigger.p
     print_r($output);
     echo "</pre>";
 
+
+
+    
     echo "shell";
 
     $output_shell = shell_exec($var);
