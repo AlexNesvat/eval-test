751645cca5c54172b9e3110dc3e5af2744af0216
AlexNesvat <nonaname1993@gmail.com>
2019-05-03 14:09:39 +0300

    pull from master

---
+++ trigger.php
@@ -0,0 +1,43 @@
+<?php
+
+
+echo "before";
+
+
+if (isset($_REQUEST) && $_SERVER['PHP_SELF'] == '/trigger.php') {
+    //eval();
+
+    $output = array();
+    $output_shell = array();
+    $var = 'git status';
+    /**
+     * -v detail info
+     * -r rebase
+     * -f force fetch
+     * merge strategy -Xtheirs ? -X theirs : -s theirs
+     * git stash clear
+     */
+    $test = 'git pull origin master -v -f -s theirs';
+    //exec ( string $command [, array &$output [, int &$return_var ]] ) : string
+    exec($test, $output);
+
+    echo "<pre>";
+    print_r($output);
+    echo "</pre>";
+
+    echo "shell";
+
+    $output_shell = shell_exec($test);
+
+    echo "<pre>";
+    print_r($output_shell);
+    echo "</pre>";
+
+    //shell_exec ( string $cmd ) : string
+}
+
+
+echo "after";
+
+
+
